7a9acba230dcf00cbc7bd680e7b5b211e9c1f272
naruapon <naruapon@webmail.npru.ac.th>
2017-10-31 15:55:01 +0700

    Initial commit

---
+++ src/InstitutionalPlan.java
@@ -0,0 +1,10 @@
+
+public class InstitutionalPlan extends Plan {
+
+	@Override
+	void getRate() {
+		// TODO Auto-generated method stub
+		rate = 5.50;
+	}
+
+}
---
+++ src/UniversityPlan.java
@@ -0,0 +1,10 @@
+
+public class UniversityPlan extends Plan {
+
+	@Override
+	void getRate() {
+		// TODO Auto-generated method stub
+		rate = 2.0;
+	}
+
+}
